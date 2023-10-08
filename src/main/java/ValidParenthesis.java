9c51054d2be6402354ce99671ec8f4c5931e743a
ShawnRSidwell <shawnrsidwell@gmail.com>
2023-10-07 21:27:39 -0700

    add valid parenthesis test

---
+++ src/main/java/ValidParenthesis.java
@@ -0,0 +1,54 @@
+
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class ValidParenthesis {
+
+    //faster, but uses more memory than method 2
+    public boolean isValid(String s) {
+        Stack<Character> bracketStack = new Stack<>();
+        for(char bracket : s.toCharArray()){
+            if(bracket == '{' || bracket == '[' || bracket == '('){
+                bracketStack.push(bracket);
+            } else {
+                if(bracketStack.isEmpty()){
+                    return false;
+                }
+                char top = bracketStack.peek();
+                if(bracket == '}' && top == '{' || bracket == ']' && top == '[' || bracket == ')' && top == '('){
+                    bracketStack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return bracketStack.isEmpty();
+    }
+
+    // Uses less memory but is slightly slower than method above 2 ms vs 1 ms, 40.2mb vs 40.9b
+    public boolean isValid2(String s) {
+        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
+        Hmap.put(')','(');
+        Hmap.put('}','{');
+        Hmap.put(']','[');
+        Stack<Character> stack = new Stack<Character>();
+        for (int idx = 0; idx < s.length(); idx++){
+
+            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
+                stack.push(s.charAt(idx));
+                continue;
+            }
+
+            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
+                return false;
+            }
+        }
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+
+}
---
+++ src/test/java/ValidParenthesisTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ValidParenthesisTest {
+
+    ValidParenthesis vp = new ValidParenthesis();
+
+    @Test
+    public void isValidTest() {
+        assertTrue(vp.isValid("()"));
+        assertTrue(vp.isValid("{}"));
+        assertTrue(vp.isValid("[]"));
+        assertTrue(vp.isValid("[][]"));
+        assertTrue(vp.isValid("[]{}()"));
+        assertTrue(vp.isValid("{}[]()(){}[]"));
+    }
+
+    @Test
+    public void isNotValidTest() {
+        assertFalse(vp.isValid("({)}"));
+        assertFalse(vp.isValid(")("));
+        assertFalse(vp.isValid("(}))))"));
+        assertFalse(vp.isValid("(})"));
+        assertFalse(vp.isValid("}"));
+        assertFalse(vp.isValid("[}"));
+        assertFalse(vp.isValid(")("));
+        assertFalse(vp.isValid("([)]"));
+
+
+
+
+
+    }
+}
